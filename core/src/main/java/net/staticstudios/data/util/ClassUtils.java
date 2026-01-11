79ef4ccd5e78073677cabcfb6195a596e362b4fe
Sammy Aknan <sammy.aknan@gmail.com>
2026-01-10 21:14:45 -0500

    fix: use data manager's class loader

--- core/src/main/java/net/staticstudios/data/util/ClassUtils.java
+++ core/src/main/java/net/staticstudios/data/util/ClassUtils.java
@@ -1,11 +1,13 @@
 package net.staticstudios.data.util;
 
+import net.staticstudios.data.DataManager;
+
 public class ClassUtils {
 
     @SuppressWarnings("unchecked")
     public static <T> Class<T> forName(String className) {
         try {
-            return (Class<T>) Class.forName(className, false, Thread.currentThread().getContextClassLoader());
+            return (Class<T>) Class.forName(className, false, DataManager.class.getClassLoader());
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Class not found: " + className, e);
         }
